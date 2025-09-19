204ebfd349a08ddf52e7c6fa07f5f4f4587a8e70
Mikhail <tyuris@icloud.com>
2025-09-19 09:44:04 +0300

    task completed

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const LAST_NAME = "Tiurin";
-const FIRST_NAME = "Mikhail";
-const PATRONYMIC = "Yurievich";
-console.log(LAST_NAME, FIRST_NAME, PATRONYMIC);
+// const LAST_NAME = "Tiurin";
+// const FIRST_NAME = "Mikhail";
+// const PATRONYMIC = "Yurievich";
+// console.log(LAST_NAME, FIRST_NAME, PATRONYMIC);
+
+const LAST_NAME = prompt('Ваше имя?')
+const FIRST_NAME = prompt('Ваше фамилия?')
+const PATRONYMIC = prompt('Ваше отчество?')
+alert(`С возвращением ${LAST_NAME} ${FIRST_NAME} ${PATRONYMIC}!`)

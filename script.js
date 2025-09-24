1bf722490fc1e5bd59e9a893d4cd904215b4a93e
Mikhail <tyuris@icloud.com>
2025-09-24 13:32:10 +0300

    task

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const num1 = Number(prompt("Give me 1st number"));
 const num2 = Number(prompt("Give me 2nd number"));
 
 const sum = (a, b) => {
-  alert(`The result is ${a + b}!`);
-  console.log(a + b);
+  alert(`The result of sum is ${a + b}!`);
+  alert(`The result of subtract is ${a - b}!`);
+  alert(`The result of multiply is ${a * b}!`);
+  alert(`The result of division is ${a / b}!`);
   return 0;
 };
 

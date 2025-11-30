96cf27f12c71a801d6273164161f941214eaae66
Michail <tyuris@icloud.com>
2025-11-30 19:21:35 +0300

    added2

--- 22-lecture-closures/src/index.js
+++ 22-lecture-closures/src/index.js
@@ -20,3 +20,18 @@ function searchProducts(products, searchWord) {
 }
 
 console.log(searchProducts(products, "Iphone"));
+
+// второй вариант без внутренней функции
+function searchProducts2(products, key) {
+  let result = [];
+  const keyWord = key.toLowerCase();
+  for (const item of products) {
+    if (item.name && item.name.toLowerCase().includes(keyWord)) {
+      result.push(item);
+    }
+    if (item.children && Array.isArray(item.children)) {
+      result.push(...searchProducts2(item.children, key))
+    }
+  }
+  return result;
+}
